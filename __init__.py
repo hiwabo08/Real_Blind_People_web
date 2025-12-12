181e79d9186f6b7d0b1f28283558717c347a4ced
hiwabo08 <hiwabo08@gmail.com>
2025-12-12 15:17:02 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,3 @@
-# # app/__init__.py
-# from flask import Flask
-
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     # Register Blueprint
-#     from .routes.main import main_bp
-#     app.register_blueprint(main_bp)
-
-#     return app
 
 
 
